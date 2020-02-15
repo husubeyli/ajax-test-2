3aec80d49a9020cdfcfd9cd9ad5cfd2339cb2646
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2020-02-15 19:33:25 +0400

    evde dersin tekrari

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
      $('#load').on('click', function(){
          $.ajax({
              url: '/ajax-test-2/item.html',
-             dataType: 'json'
+             dataType: 'json',
          }).done(function(response){
-             console.log(response)
+            console.log(response)
             $('#siyahi').empty();
             response.images.forEach(function(value, index){
                 $('#siyahi').append('<img src= "' + value + '">')
