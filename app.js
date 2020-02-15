aa651c800175a4efe9055ae8cd4cbbfa57cfe77e
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2020-02-15 18:50:42 +0400

    evde dersin tekrari

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-$(document).o
+$(document).ready(function(){
+     $('#load').on('click', function(){
+         $.ajax({
+             url: '/ajax-test-2/item.html',
+             dataType: 'json'
+         }).done(function(reponse){
+            $('#siyahi').empty();
+            reponse.images.forEach(element => {
+                $('#siyahi').append('<img src="' + value + '">')
+            });
+         })
+     })
+})
