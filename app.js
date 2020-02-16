77e5830d7f61fa3ea36460ca691f8bad7d648181
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2020-02-17 03:56:58 +0400

    evde dersin tekrari

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
              url: '/ajax-test-2/item.html',
              dataType: 'json'
          }).done(function(response){
-                // if(response.backgroundWhite){
-                //     response.backgroundBlack
-                // }else {
-                //     response.backgroundWhite
-                // }
+                if(response.backgroundWhite){
+                    response.backgroundBlack
+                }else {
+                    response.backgroundWhite
+                }
                 $('body').css({
                     background: response.backgroundBlack
                 })
