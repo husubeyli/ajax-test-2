f1b9a55473fc33e813bfd930c4672e9661e97b21
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2020-02-17 04:51:28 +0400

    ikidene ajax

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 $(document).ready(function(){
-     $('#changeThem').on('click', function(){
-         $.ajax({
+     
+         var ajax1 = $('#changeThem').on('click', function(){ $.ajax({
              url: '/ajax-test-2/item.html',
              dataType: 'json'
-         }).done(function(response){
-                $('body').css({
-                    background: response.backgroundBlack
-                })
-
-         })
      })
-     $('#load').on('click', function(){
-         let title_film = $('#title_film').val()
-         $.ajax({
+    
+         var ajax2 =  $('#load').on('click', function(){
+            let title_film = $('#title_film').val() $.ajax({
              dataType: 'json',
              url: 'http://omdbapi.com/?t=' +title_film + '&apikey=62997bce'
-         }).done(function(response){
-             $('#poster').attr('src', response.Poster),
-             $('#info #header').html(`Film name: ${response.Title}`),
-             $('#country').html(`Country: ${response.Country}`),
-             $('#year').html(`Year: ${response.Year}`)
          })
-     })
+    })
+         $.when(ajax1, ajax2 ).done(function(response1, response2){
+            $('body').css({
+                background: response1.backgroundBlack
+            })
+             $('#poster').attr('src', response2.Poster),
+             $('#info #header').html(`Film name: ${response2.Title}`),
+             $('#country').html(`Country: ${response2.Country}`),
+             $('#year').html(`Year: ${response2.Year}`)
+         })
+
 })
 
 
